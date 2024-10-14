32e121cb93800bf5d8b2f5f75c212b9e65f62b90
koralle <mkhd7930@gmail.com>
2024-10-14 22:22:47 +0900

    feat: setup with Astro

---
+++ astro.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config'
+
+import mdx from '@astrojs/mdx'
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'static',
+
+  prefetch: true,
+
+  vite: {
+    build: {
+      rollupOptions: {}
+    }
+  },
+
+  integrations: [mdx(), react()],
+
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'catppuccin-frappe',
+        dark: 'catppuccin-mocha'
+      }
+    }
+  }
+})
